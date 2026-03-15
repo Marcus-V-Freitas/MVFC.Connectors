d8b8de84ebe6db2bf82504cf5cecbead27fd0340
Marcus Costa <53195917+Marcus-V-Freitas@users.noreply.github.com>
2026-03-15 20:02:30 -0300

    testes

--- src/MVFC.Connectors.Developer/MysqlExplain/IMysqlExplainApi.cs
+++ src/MVFC.Connectors.Developer/MysqlExplain/IMysqlExplainApi.cs
@@ -1,5 +1,10 @@
 ﻿namespace MVFC.Connectors.Developer.MysqlExplain;
 
+[Headers(
+    "User-Agent: Mozilla/5.0 (compatible; MyApp/1.0)",
+    "Accept: application/json",
+    "Content-Type: application/json"
+)]
 public interface IMysqlExplainApi : IConnectorApi
 {
     [Post("/explains")]
